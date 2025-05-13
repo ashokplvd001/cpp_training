2efafb995173bce134c929907bc39ea7c8614b89
ashokplvd001 <ashokplvd001@gmail.com>
2025-05-13 12:01:21 +0530

    update

--- assessments/day20/header.h
+++ assessments/day20/header.h
@@ -2,11 +2,16 @@
 #include <iostream>
 
 #define MONTH_MAX 31 
- 
-int** MonthArray = nullptr ; 
-int* dayArray = nullptr ; 
-int* sumArray = nullptr ; 
-int dayCount = 0; 
+#define DAY_ARRAY_SIZE 4 
+
+
+int** monthArray = nullptr;
+int* sumArray = nullptr;
+int dayCount = 0;
+
+char * food[DAY_ARRAY_SIZE] = { "BreakFast" , "Lunch" , "Dinner" , "Others " };
+
+
 
 void* createMemSpace(void* ptr, int dataTypeSize, int sizeOfArra);
 
